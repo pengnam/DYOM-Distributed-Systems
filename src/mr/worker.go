88abb9bfb6d21fc7b5540ef2d3d6a2f854c332f7
pengnam <seanngpengnam@gmail.com>
2020-01-11 21:36:14 +0800

    Initial commit

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -31,7 +31,7 @@ func Worker(mapf func(string, string) []KeyValue,
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the master.
-	// CallExample()
+	CallExample()
 
 }
 
